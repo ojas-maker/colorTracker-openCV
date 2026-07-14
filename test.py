549faedf279c03a390df3d2e8dd97e8ab06575d1
ojas-maker <ojas7231@gmail.com>
2026-07-14 17:12:05 +0530

    hand tracking( mediapipe ) test code

--- test.py
+++ test.py
@@ -1,13 +1,39 @@
 import cv2
+import mediapipe as mp
+import numpy as np
 
-cam = cv2.VideoCapture(0)
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.7)
+mp_draw = mp.solutions.drawing_utils 
+
+cap = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cam.read()
-    cv2.imshow("Camera", frame)
-
-    if cv2.waitKey(1) == ord('w'):
+    success, frame = cap.read()
+    if not success:
         break
 
-cam.release()
+    img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(img_rgb)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            
+            mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+            index_tip = hand_landmarks.landmark[8]
+
+            h, w, c = frame.shape
+
+            cx = int(index_tip.x * w)
+            cy = int(index_tip.y * h)
+
+            print(f"Index Finger is at X: {cx} pixels | Y: {cy} pixels")
+
+    cv2.imshow("computer Vision - Day 2", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
 cv2.destroyAllWindows()
